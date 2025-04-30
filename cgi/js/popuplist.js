2504964358e06f9dc7d0eeded8680216faf581e8
Waqas Rahman <waqasrehman@invotyx.co.uk>
2025-04-30 14:23:34 +0500

    editor changes

--- cgi/js/popuplist.js
+++ cgi/js/popuplist.js
@@ -1,20 +1,22 @@
 // both these functions use document.FORM...
 // keep the FORM name the same because it is hardcoded.
-function allergylist(selectobj)
-{
-  // values at load of page 
-  this.idx = selectobj.selectedIndex;
-  this.value = selectobj.options[this.idx].value;
-  this.text = selectobj.options[this.idx].text;
-//alert("idx="+this.idx+" value="+this.value+" text="+this.text);
-  this.set = function(pattern)
-  {
-    var req = new AjaxRequest();
-    var script = "/cgi/bin/popup.pl";
-    var params = "method=pAllergy&pattern=" + encodeURIComponent(pattern) + "&value=" + document.ClientAllergies.ClientAllergies_AID_1.value;
-//alert("url="+script+" params="+params+" value="+this.value);
-    req.loadXMLDoc(script, params);
-  }
+function allergylist(selectobj) {
+	// values at load of page
+	this.idx = selectobj.selectedIndex;
+	this.value = selectobj.options[this.idx].value;
+	this.text = selectobj.options[this.idx].text;
+	//alert("idx="+this.idx+" value="+this.value+" text="+this.text);
+	this.set = function (pattern) {
+		var req = new AjaxRequest();
+		var script = "/cgi/bin/popup.pl";
+		var params =
+			"method=pAllergy&pattern=" +
+			encodeURIComponent(pattern) +
+			"&value=" +
+			document.ClientAllergies.ClientAllergies_AID_1.value;
+		//alert("url="+script+" params="+params+" value="+this.value);
+		req.loadXMLDoc(script, params);
+	};
 }
 function problemlist(selectobj) {
 	// values at load of page
