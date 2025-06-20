4b2b7db274d97ed43c55de3ecb7dd4a2cef08889
Waqas Rahman <waqasrehman@invotyx.co.uk>
2025-06-20 16:11:24 +0500

    Refactor tst.pl to improve structure and readability

--- cgi/bin/tst.pl
+++ cgi/bin/tst.pl
@@ -1,17 +1,34 @@
 #!/usr/bin/perl
-#############################################################################
-print "ENV=".$ENV{DOCUMENT_ROOT}."\n";
-foreach $var (sort keys %ENV) { print "$var = $ENV{$var}<BR>\n"; }
+use lib '/var/www/okmis/src/lib';
+use DBA;
+use myForm;
+use myDBI;
+############################################################################
+my $form = myForm->new();
+my $dbh  = myDBI->dbconnect( $form->{'DBNAME'} );
+
+#&Forms::FormGet(\%FORM);
+############################################################################
+my $isHTML = 0;
+if ( $ENV{HTTP_USER_AGENT} ) { $isHTML = 1; }
+print "content-type: text/html\n\n";
+print "ENV=" . $ENV{DOCUMENT_ROOT} . "<BR>\n";
+
+foreach $var ( sort keys %ENV ) { print "$var = $ENV{$var}<BR>\n"; }
+
 #use lib "$ENV{DOCUMENT_ROOT}/lib";
-use lib "$ENV{MISLIB}";
-use DBUtil;
+# use lib "$ENV{MISLIB}";
+# use DBUtil;
 
 #############################################################################
-my $DT=localtime();
+my $DT = localtime();
 warn qq|DT=$DT\n|;
 my $html = qq|ENV=$ENV{DOCUMENT_ROOT}<BR>IT WORKED|;
-print qq|Content-type: text/html\n\n<HTML>\n<HEAD><TITLE>TEST HTML</TITLE></HEAD>\n<BODY >\n|
-      . $html . qq|\n</BODY>\n</HTML>\n|;
-exit;
+print
+qq|Content-type: text/html\n\n<HTML>\n<HEAD><TITLE>TEST HTML</TITLE></HEAD>\n<BODY >\n|
+  . $html
+  . qq|\n</BODY>\n</HTML>\n|;
 
-#############################################################################
+myDBI->cleanup();
+exit;
+############################################################################
